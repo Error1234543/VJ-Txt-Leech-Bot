13e256e8b1106ae67bebb9f3ccd9ba54979cbc6e
Error1234543 <ptaviyad21@gmail.com>
2025-06-08 13:52:59 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,4 +6,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "20619533"))
 API_HASH = environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8176277339:AAFwc7xY8cTWyUNVm3Q3_HkyJXTWRyH_kkM")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7270825299:AAFGI_JSn7AXjejfhPzZkPQHWa_gCDVfoP8")
