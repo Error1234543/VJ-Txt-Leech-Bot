e023adeef1f31d04fc67c67dc4831f8f92f15d86
Error1234543 <ptaviyad21@gmail.com>
2025-06-11 10:03:32 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,4 +6,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "20619533"))
 API_HASH = environ.get("API_HASH", "5893568858a096b7373c1970ba05e296")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8025071282:AAENA039TZUh2rjt7wNpYTxe1jjlAZHLkGw")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7764590689:AAFc4kG8_8hBRjye9MdsMndgwTfEPisSohE")
